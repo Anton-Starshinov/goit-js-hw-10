047be3884dd1b4de7ab925a16af9ce9a4b6157b1
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-08-19 17:57:28 +0300

    save 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch(
+  'https://restcountries.com/v2/all?fields=name,capital,population,flags.svg,languages'
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(cuntry => {
+    console.log(cuntry);
+  })
+  .catch(error => {
+    console.log(error);
+  });
