07b5cd3a9cc20723c831e4f2ad876d273fb5f4b9
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-08-20 17:54:20 +0000

    Deploying to gh-pages from @ Anton-Starshinov/goit-js-hw-10@c6d8c218512b22b23993928e4269d10c05bca1de 🚀

--- index.2e1b5f5d.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/all?fields=name,capital,population,flags.svg,languages").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.2e1b5f5d.js.map
--- index.35d59ac1.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/all?fields=name,capital,population,flags.svg,languages").then((l=>l.json())).then((l=>{console.log(l)})).catch((l=>{console.log(l)}));
-//# sourceMappingURL=index.35d59ac1.js.map
