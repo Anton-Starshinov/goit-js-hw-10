c6d8c218512b22b23993928e4269d10c05bca1de
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-08-20 20:53:51 +0300

    finish

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+// // function fetchCountries(name) {
+// //     return
+// fetch(
+//   'https://restcountries.com/v3.1/name/peru?fields=name,capital,population,flags.svg,languages'
+// )
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(cuntry => {
+//     console.log(cuntry);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+// // }
+// // fetchCountries();
--- src/index.js
+++ src/index.js
@@ -1,16 +1,68 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch(
-  'https://restcountries.com/v2/all?fields=name,capital,population,flags.svg,languages'
-)
-  .then(response => {
+const inputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+  const searchQuery = evt.target.value.trim();
+
+  fetchCountries(searchQuery)
+    .then(blockSelection)
+    .catch(Notify.failure('Oops, there is no country with that name'));
+}
+
+function blockSelection(cuntry) {
+  if (cuntry.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+  if (cuntry.length > 1 && cuntry.length < 10) {
+    markupCountyList(cuntry);
+    countryInfoEl.innerHTML = '';
+  }
+  if (cuntry.length === 1) {
+    countryListEl.innerHTML = '';
+    markupCountyInfo(cuntry);
+  }
+}
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
     return response.json();
-  })
-  .then(cuntry => {
-    console.log(cuntry);
-  })
-  .catch(error => {
-    console.log(error);
   });
+}
+
+function markupCountyList(cuntry) {
+  const markup = cuntry
+    .map(({ flags, name }) => {
+      return `
+      <li><img src="${flags.svg}" alt="${name}" width="35" height="35" />"${name.official}"</li>`;
+    })
+    .join('');
+
+  countryListEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function markupCountyInfo(cuntry) {
+  const markupInfo = cuntry
+    .map(({ flags, name, capital, population, languages }) => {
+      return `
+          <h1><img src="${flags.svg}" alt="${name}" width="35" height="35" />"${name.official}"</h1>
+          <ul class="country-list">
+            <li><p>Capital: ${capital}</p></li>
+            <li><p>Population: ${population}</p></li>
+            <li><p>Languages: ${languages}</p></li>
+          </ul>`;
+    })
+    .join('');
+
+  countryInfoEl.insertAdjacentHTML('beforeend', markupInfo);
+}
